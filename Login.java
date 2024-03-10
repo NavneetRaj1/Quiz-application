928686bae1196ecae984e6b433a71f5ae9c8d644
NavneetRaj1 <133239634+NavneetRaj1@users.noreply.github.com>
2024-03-11 00:48:32 +0530

    Add files via upload

--- Login.java
+++ Login.java
@@ -1,70 +1,72 @@
-package quizapplication;
-
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.*;
-
+package quizapp;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login extends JFrame  implements ActionListener{
-    JButton Rules,Back;
-    Login() {
-        getContentPane().setBackground(Color.lightGray);
+public class Login extends JFrame implements ActionListener {
+    JTextField text;
+    JButton Next, Exit;
+
+    Login(){
+
+        getContentPane().setBackground(Color.WHITE);
         setLayout(null);
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("login.jpeg"));
-        JLabel image = new JLabel(i1);
-        image.setBounds(0,0,600,500);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Login.png"));
+        Image i = i1.getImage().getScaledInstance(1920,1080,Image.SCALE_DEFAULT);
+        ImageIcon i2 = new ImageIcon(i);
+        JLabel image = new JLabel(i2);
+        image.setBounds(1000,0,1220,1080);
         add(image);
 
-        JLabel heading = new JLabel("Simpile Minds");//fro showing somethinmg on heading
-        heading.setBounds(750,60,300,45);
-        heading.setFont(new Font("vivaldi",Font.BOLD,49));
-        heading.setForeground(new Color(10,150,140));
+        JLabel heading = new JLabel("QUIZ TIME");
+        heading.setBounds(140,60,300,45);
+        heading.setFont(new Font("Viner Hand ITC",Font.BOLD,40));
+        heading.setForeground(new Color(22,99,70));
         add(heading);
 
-        JLabel name = new JLabel("Enter Your Name");//fro showing somethinmg on heading
-        name.setBounds(810,150,300,20);
-        name.setFont(new Font("Mongolian Bait",Font.BOLD,18));
-        name.setForeground(new Color(10,110,140));
+        JLabel name = new JLabel("Enter Your Name");
+        name.setBounds(160,150,300,20);
+        name.setFont(new Font("Mongolian Baiti", Font.BOLD,18));
+        name.setForeground(new Color(80,150,54));
         add(name);
 
-        JTextField tfname = new JTextField();
-        tfname.setBounds(735,200,300,25);
-        tfname.setFont(new Font("Times  New Roman",Font.BOLD,15));
-        add(tfname);
-        
-        Rules = new JButton("Rules");
-        Rules.setBounds(735,270,120,25);
-        Rules.setBackground(new Color(20,144,120));
-        Rules.setForeground(Color.black);
-        Rules.addActionListener(this);
-        add(Rules);
+        text = new JTextField();
+        text.setBounds(80,200,300,25);
+        text.setFont(new Font("Times New Roman", Font.BOLD,18));
+        name.setForeground(new Color(80,150,54));
+        add(text);
 
-        Back = new JButton("Back");
-        Back.setBounds(912,270,120,25);
-        Back.setBackground(new Color(20,144,120));
-        Back.setForeground(Color.black);
-        Back.addActionListener(this);
-        add(Back);
+        Next = new JButton("Next");
+        Next.setBounds(100,270,120,25);
+        Next.setBackground(new Color(22,99,54));
+        Next.setForeground(Color.WHITE);
+        Next.addActionListener(this);
+        add(Next);
 
+        Exit = new JButton("EXit");
+        Exit.setBounds(250,270,120,25);
+        Exit.setBackground(new Color(22,99,54));
+        Exit.setForeground(Color.WHITE);
+        Exit.addActionListener(this);
+        add(Exit);
 
-        setSize(1200,500);
-        setLocation(200,150);
+        setSize(1920,1080);
+//        setLocation(200,220);
+//        setUndecorated(true);
         setVisible(true);
 
-        // JOptionPane.showInputDialog(heading, image);
     }
 
-     @Override
-    public void actionPerformed(ActionEvent ae) {
-        // TODO Auto-generated method stub
-        // throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-        if (ae.getSource() ==Rules){
-
-        }
-        else if (ae.getSource() == Back){
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == Next){
+            String name = text.getText();
             setVisible(false);
+            new Rules(name);
+        } else if (e.getSource() == Exit) {
+            System.exit(50);
         }
     }
     public static void main(String[] args) {
